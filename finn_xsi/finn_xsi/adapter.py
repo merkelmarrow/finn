240dd536c02a71ecb304f1aa5130f3561d776d79
auphelia <jakobapk@web.de>
2026-04-21 11:11:34 +0100

    [finnxsi] Add mvu_pkg to be loaded first

--- finn_xsi/finn_xsi/adapter.py
+++ finn_xsi/finn_xsi/adapter.py
@@ -45,9 +45,12 @@ def compile_sim_obj(top_module_name, source_list, sim_out_dir, debug=False, beha
         }
         verilog_header_incl_str = " ".join(["--include " + x for x in verilog_headers])
 
-        # sort src list so that configs are loaded first
-        pattern = "swg_pkg"
-        srcs_list = sorted(source_list, key=lambda s: (pattern not in s, s))
+        # sort src list so that packages are loaded first
+        # these packages must be compiled before modules that depend on them
+        pkg_patterns = ["swg_pkg", "mvu_pkg"]
+        srcs_list = sorted(
+            source_list, key=lambda s: (not any(pkg in s for pkg in pkg_patterns), s)
+        )
         for src_line in srcs_list:
             if src_line.endswith(".v"):
                 f.write(f"verilog work {verilog_header_incl_str} {src_line}\n")
